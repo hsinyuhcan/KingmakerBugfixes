e9542ce0ee4f96d58699bf3d99df7e029a09e8cb
Kim Kræmer <kim.kraemer@gmail.com>
2019-07-24 16:45:11 +0200

    Basic template

---
+++ KingmakerBugfixes/Main.cs
@@ -0,0 +1,31 @@
+﻿using Harmony12;
+using UnityModManagerNet;
+
+namespace Bugfixes
+{
+    public class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool active)
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            if (active)
+            {
+                harmony.PatchAll();
+            }
+            else
+            {
+                harmony.UnpatchAll();
+            }
+            return true;
+        }
+    }
+}
